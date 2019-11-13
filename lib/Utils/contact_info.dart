eb75dbe07dd6eb8f1cdebdd1e7e4abf717e6fd5e
Kshitij Passi <kshitijnishu@gmail.com>
2019-11-13 21:33:27 +0530

    contact fix

--- lib/Utils/contact_info.dart
+++ lib/Utils/contact_info.dart
@@ -1,4 +1,3 @@
-import 'dart:html' as html;
 import 'package:flutter_web/material.dart';
 import 'package:responsive_porfolio/responsive_widget.dart';
 
@@ -9,57 +8,57 @@ class ContactInfo extends StatefulWidget {
 
 class _ContactInfoState extends State<ContactInfo> {
   projectCards() => new Container(
-    height: ResponsiveWidget.isSmallScreen(context) ? 600 : 600,
-    width: ResponsiveWidget.isSmallScreen(context) ? 700 : 700,
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          "CONTACT ME",
-          textScaleFactor: 3.0,
-          style: TextStyle(color: Colors.blue),
-        ),
-        SizedBox(
-          height: 50,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        height: ResponsiveWidget.isSmallScreen(context) ? 600 : 600,
+        width: ResponsiveWidget.isSmallScreen(context) ? 700 : 700,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              "- Email : kshitijnishu@gmail.com",
-              textScaleFactor: 2.0,
-              style: TextStyle(color: Colors.white),
+              "CONTACT ME",
+              textScaleFactor: 3.0,
+              style: TextStyle(color: Colors.blue),
             ),
-            Text(
-              "- Github : @kshitij-25",
-              textScaleFactor: 2.0,
-              style: TextStyle(color: Colors.white),
+            SizedBox(
+              height: 50,
             ),
-            Text(
-              "- LinkedIn : @kshitij-passi",
-              textScaleFactor: 2.0,
-              style: TextStyle(color: Colors.white),
-            ),
-            Text(
-              "- Facebook : @kshitijpassi",
-              textScaleFactor: 2.0,
-              style: TextStyle(color: Colors.white),
-            ),
-            Text(
-              "- Twitter : @kshitijpassi",
-              textScaleFactor: 2.0,
-              style: TextStyle(color: Colors.white),
-            ),
-            Text(
-              "- Instagram : @kshitij.passi",
-              textScaleFactor: 2.0,
-              style: TextStyle(color: Colors.white),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "- Email : kshitijnishu@gmail.com",
+                  textScaleFactor: 2.0,
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text(
+                  "- Github : @kshitij-25",
+                  textScaleFactor: 2.0,
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text(
+                  "- LinkedIn : @kshitij-passi",
+                  textScaleFactor: 2.0,
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text(
+                  "- Facebook : @kshitijpassi",
+                  textScaleFactor: 2.0,
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text(
+                  "- Twitter : @kshitijpassi",
+                  textScaleFactor: 2.0,
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text(
+                  "- Instagram : @kshitij.passi",
+                  textScaleFactor: 2.0,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
             ),
           ],
         ),
-      ],
-    ),
-  );
+      );
 
   @override
   Widget build(BuildContext context) {
