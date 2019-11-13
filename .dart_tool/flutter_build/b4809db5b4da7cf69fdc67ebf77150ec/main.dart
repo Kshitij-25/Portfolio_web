fc778b50f57670b8c9cea9565ab24c697ee356ad
Kshitij Passi <kshitijnishu@gmail.com>
2019-11-13 23:04:21 +0530

    changes

---
+++ .dart_tool/flutter_build/b4809db5b4da7cf69fdc67ebf77150ec/main.dart
@@ -0,0 +1,10 @@
+import 'dart:ui' as ui;
+
+import "file:///C:/Users/Kshitij/Desktop/portfolio_web/lib/main.dart" as entrypoint;
+
+Future<void> main() async {
+  if (true) {
+    await ui.webOnlyInitializePlatform();
+  }
+  entrypoint.main();
+}
